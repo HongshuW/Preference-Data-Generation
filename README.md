5d9fdae52d2fc6bb2903d43ec00dc40343eecf7a
HongshuW <hongshu.wang.uni@gmail.com>
2025-03-30 22:40:48 +0800

    check images with missing labels

--- README.md
+++ README.md
@@ -7,18 +7,23 @@ This repository is for CS6240 course project.
 |- download_data\
 &nbsp;&nbsp;|- load_data.py\
 &nbsp;&nbsp;|- load_data_narratives.py\
+&nbsp;&nbsp;|- filter_meta_files.py\
+&nbsp;&nbsp;|- find_images_with_missing_labels.py\
 |- process_data.py\
 |- README.md
 
 ### Data
 - The `data` folder contains a benchmark of 100 images retrieved from Open Images V7 dataset. This folder contains all the data used for this project.
+- The dataset can be used directly, to replicate the dataset, please refer to "Data Replication".
 
 ### Data Replication
+To replicate the dataset, execute the scripts in the following order.
 Modify the `custom_dir` variable to download to a custom folder in the following files:
 
 - `load_data.py` downloads images with `detections`, `classifications`, `relationships` labels. 
 - `load_data_narratives.py` downloads narratives of the images.
 - `filter_meta_files.py` filter the labels by downloaded image IDs.
+- `find_images_with_missing_labels.py` checks and records which images contain missing labels.
 
 ### Baseline
 TODO
---
+++ download_data/find_images_with_missing_labels.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import os
+
+custom_dir = "C:\\Users\\admin\\repos\\Preference-Data-Generation\\data"
+inner_meta_path = "\\open-images-v7\\train\\metadata\\"
+inner_labels_path = "\\open-images-v7\\train\\labels\\"
+
+
+# Load metadata
+classifications_df = pd.read_csv(custom_dir + inner_labels_path + 'filtered_classifications.csv')
+classification_image_ids = set(classifications_df['ImageID'].unique())
+
+detections_df = pd.read_csv(custom_dir + inner_labels_path + 'filtered_detections.csv')
+detection_image_ids = set(detections_df['ImageID'].unique())
+
+relationships_df = pd.read_csv(custom_dir + inner_labels_path + 'filtered_relationships.csv')
+relationship_image_ids = set(relationships_df['ImageID'].unique())
+
+# read image IDs
+with open(custom_dir + inner_meta_path + "downloaded_image_ids.txt") as f:
+    all_image_ids = set(line.strip() for line in f)
+
+
+# Determine Missing Image IDs for Each Metadata File
+missing_from_classifications = all_image_ids - classification_image_ids
+missing_from_detections = all_image_ids - detection_image_ids
+missing_from_relationships = all_image_ids - relationship_image_ids
+
+def write_missing_ids(missing_ids, filename):
+    with open(filename, 'w') as f:
+        for image_id in sorted(missing_ids):
+            f.write(f"{image_id}\n")
+
+# Save missing image IDs
+write_missing_ids(missing_from_classifications, custom_dir + inner_meta_path + 'missing_from_classifications.txt')
+write_missing_ids(missing_from_detections, custom_dir + inner_meta_path + 'missing_from_detections.txt')
+write_missing_ids(missing_from_relationships, custom_dir + inner_meta_path + 'missing_from_relationships.txt')
